8aeb1a31f5a7b2dbd7b3840a2bfe76a76fdc31ad
Alam Sizaguirre <asizaguirre@gmail.com>
2026-01-30 10:56:47 -0300

    Deploy: Atualização automática para produção

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL, // ✅ opcional, reforça a URL
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
